971b7633dc3387aee9a393334b101ff2c6de49da
impressivecoder1 <rohanrahman251@gmail.com>
2026-02-28 00:15:52 +0600

    end of class six

--- src/components/Product/ProductForm.jsx
+++ src/components/Product/ProductForm.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const ProductForm = () => {
+const ProductForm = ({handleAddProduct}) => {
+    const [error, setError] = useState('')
     const handleProductSubmit = e => {
         e.preventDefault()
         // console.log(e.target.value)
@@ -8,22 +9,41 @@ const ProductForm = () => {
         const price = e.target.price.value
         const quantity = e.target.quantity.value
         // console.log(name, price, quantity)
+         if(name.length === 0){
+            setError('Please Provide a Product Name')
+            return
+        }
+        else if(price.length === 0){
+            setError('Please Provide a Price')
+            return
+        }
+        else if(price < 0) {
+            setError('Price can not be negative')
+            return
+        }
+        else{
+            setError('')
+        }
         const newProduct = {
             name: name,
             price: price,
             quantity: quantity
         }
-        console.log(newProduct)
+      
+         handleAddProduct(newProduct)
+       
     }
     return (
         <div>
             <h1>Add a Product</h1>
             <form onSubmit={handleProductSubmit}>
                 <input type="text" name= 'name' placeholder='Product Name' /> <br />
-                <input type="text" name= 'price' placeholder='Product Price Name' /> <br />
+                <input type="number" name= 'price' placeholder='Product Price Name' /> <br />
                 <input type="number" name= 'quantity' placeholder='Product Quantity' /> <br />
                 <input type="submit" value="Submit" />
             </form>
+            <p style={{color : 'red'}}><small>{error}</small></p>
+            
         </div>
     );
 };
--- src/components/Product/ProductTable.jsx
+++ src/components/Product/ProductTable.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
-const ProductTable = () => {
+const ProductTable = ({products}) => {
     return (
         <div>
-            
+            <p>Products: {products.length}</p>
+            <table>
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>Product</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        products.map((product, index) => <tr product={product} key={index}>
+                        <td>{index +1}</td>
+                        <td>{product.name}</td>
+                        <td>{product.price}</td>
+                        <td>{product.quantity}</td>
+                        <td></td>
+                    </tr>)
+                    }
+                </tbody>
+            </table>
         </div>
     );
 };
